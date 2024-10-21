50974a637864cd57006f06e3bb3f495b0699a3a5
Joshua Ackermann <jackermann@abilityhomehealthservices.com>
2024-10-21 15:59:46 -0400

    edited download

--- ab‎ility_extraction/visit_note_scheduler_checker/visit_note.py
+++ ab‎ility_extraction/visit_note_scheduler_checker/visit_note.py
@@ -9,10 +9,10 @@ from pathlib import Path
 import shutil
 
 class ReportDownloader:
-    def __init__(self):
+    def __init__(self, target_folder = None):
         # Get the system's default download folder
         self.download_folder = self.get_default_download_folder()
-        self.target_folder = "/path/to/target/folder"  # Set your target folder for moved files
+        self.target_folder = target_folder or f"C:\\Users\\{os.getlogin()}\\Downloads"  # Set your target folder for moved files
         self.dfs = []  # To store the dataframes after loading
 
         # Set Chrome options to use the default download folder
@@ -92,8 +92,12 @@ class ReportDownloader:
         start_date, end_date = self.get_previous_month_range()
 
         # URLs
-        admission_base_url = "https://aloraplus.com/Report/RptAdmDetail?dateFrom={}&dateThrough={}"
-        visit_list_base_url = "https://aloraplus.com/Report/RptVisitList?dateFrom={}&dateThrough={}"
+        admission_base_url = "https://aloraplus.com/Report/RptAdmDetail?dateFrom={}&dateThrough={}&rdOffice=0&officesList_length=10&patient=&patientName=&patientAll=true&rdPayer=1&payersList_length=10&pay-42=42&groupBy=N&includeAdmBefore=true&showFrequency=true&caregiver=&caregiverName=&caseManagerAll=true&physician=&physicianName=&physicianAll=true&evacLevel=&evacLevelName=&evacLevelAll=true&region=&regionName=&regionAll=true&careTeamMember=&careTeamMemberAll=true&admTrack1Num=&displayTelephony=true&cdIdsArray=13%2C41%2C9%2C42&officeIdsArray=&admTrack1=&admTrack2=&admTrack3=&admTrack4=&admTrack5=&admTrack6=&optSettingName=&setPublic=1&DataField=&Relational=&UserText=&optSettings_length=10&reportType=AdmDetail&optSettings=%5B%5D"
+        visit_list_base_url = "https://aloraplus.com/Report/RptVisitList?dateFrom={}&dateThrough={}}&patient=&patientAll=true&caregiverVisit=&caregiverAll=true&visitStatus=All&groupBy=P&sortBy=VD&office=&officeAll=true&billingCode=&billingCodeAll=true&allDisciplines=true&disciplineNameOrType=N&discipline=-1&payer=&payerAll=true&payrollStatus=A&includeAmount=None&billingStatus=A&billable=A&patientRegion=&patientRegionAll=true&patientName=&officeName=&caregiverName=&billingCodeName=&payerName=&regionName=&disId=&disText=&cdIdsArray=undefined"
+        physical_therapy_docs_base_url = "https://aloraplus.com/PT/Summary?id={}"
+        occupational_therapy_docs_base_url = "https://aloraplus.com/OT/Summary?id={}"
+        speech_therapy_docs_base_url = "https://aloraplus.com/ST/STSummary?id={}"
+        medical_social_worker_docs_base_url = "https://aloraplus.com/MedicalSocialWorker/Summary?id={}"
 
         updated_admission_url = admission_base_url.format(start_date, end_date)
         updated_visit_list_url = visit_list_base_url.format(start_date, end_date)
